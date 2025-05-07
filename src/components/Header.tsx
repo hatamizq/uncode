47fd5d0051854473fa18e7947f19769844a38427
zaqi <muzaqi.hatami@amarbank.co.id>
2025-05-07 13:36:53 +0700

    fix: fix routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,13 @@ export function Header() {
 	return (
 		<header>
 			<nav>
-				<a href="/" class={url == '/' && 'active'}>
+				<a href="/uncode" class={url == '/' && 'active'}>
 					Home
 				</a>
-				<a href="/text" class={url == '/text' && 'active'}>
+				<a href="/uncode/text" class={url == '/text' && 'active'}>
 					Base64encode
 				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
+				<a href="/uncode/404" class={url == '/404' && 'active'}>
 					404
 				</a>
 			</nav>
